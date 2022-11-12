75fd1f0ef7ff9700d91773f4444f8ba5798c4907
lulzette <ivan@lulzette.ru>
2022-11-12 14:31:12 +0300

    Comments

--- main.tf
+++ main.tf
@@ -1,3 +1,6 @@
+
+# Define and Initialize provider
+
 terraform {
   required_providers {
     yandex = {
@@ -10,6 +13,40 @@ terraform {
 provider "yandex" {
   zone = "ru-central1-a"
 }
+
+
+# Create network and subnetwork in it, add external ipv4 address
+
+resource "yandex_vpc_network" "foo" {}
+
+resource "yandex_vpc_subnet" "foo" {
+  zone           = "ru-central1-a"
+  v4_cidr_blocks = ["10.2.0.0/16"]
+  network_id     = yandex_vpc_network.foo.id
+}
+
+resource "yandex_vpc_address" "addr" {
+  name = "otus-vm-address"
+
+  external_ipv4_address {
+    zone_id = "ru-central1-a"
+  }
+}
+
+# Get OS Image UUID
+
+data "yandex_compute_image" "my_image" {
+  family = var.vmimage
+}
+
+# Ansible inventory file
+
+resource "local_file" "ansible_inventory" {
+    content = "otus-vm ansible_host=${yandex_vpc_address.addr.external_ipv4_address[0].address} ansible_ssh_common_args='-o StrictHostKeyChecking=no' ansible_ssh_user=ubuntu"
+    filename = "./inventory"
+}
+
+# Create VM
 
 resource "yandex_compute_instance" "otus-vm" {
   name                      = var.vmname
@@ -33,40 +70,20 @@ resource "yandex_compute_instance" "otus-vm" {
     nat            = true
   }
 
+  # Place our ssh key in VM 
   metadata = {
     ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
   }
+
+  # Skip image ID changes in order not to reinstall entire VM
   lifecycle {
     ignore_changes = [boot_disk[0].initialize_params[0].image_id]
   }
 
 }
 
-resource "yandex_vpc_network" "foo" {}
 
-resource "yandex_vpc_subnet" "foo" {
-  zone           = "ru-central1-a"
-  v4_cidr_blocks = ["10.2.0.0/16"]
-  network_id     = yandex_vpc_network.foo.id
-}
-
-resource "yandex_vpc_address" "addr" {
-  name = "otus-vm-address"
-
-  external_ipv4_address {
-    zone_id = "ru-central1-a"
-  }
-}
-
-data "yandex_compute_image" "my_image" {
-  family = var.vmimage
-}
-
-resource "local_file" "inv" {
-    content = "otus-vm ansible_host=${yandex_vpc_address.addr.external_ipv4_address[0].address} ansible_ssh_common_args='-o StrictHostKeyChecking=no' ansible_ssh_user=ubuntu"
-    filename = "./inventory"
-}
-
+# Output public ipv4
 
 output "ipv4_address" {
   description = "IP Address of created VM"
